2b2e0c979e1939529fdf569425bccae4886d6d8f
Anshugood123 <91580123+Anshugood123@users.noreply.github.com>
2021-12-12 15:58:21 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,7 +6,7 @@ else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /KatilHassena
 fi
-cd /EvaMaria
+cd /katilHassena
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
